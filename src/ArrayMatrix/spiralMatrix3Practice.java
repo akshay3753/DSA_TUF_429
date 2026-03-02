de4e1f37e51faa8dd78f917f6ff2850de6d75563
Akshay <akshay3753@gmail.com>
2026-03-02 20:47:19 +0530

    Array's Matrix Problem

---
+++ src/ArrayMatrix/spiralMatrix3Practice.java
@@ -0,0 +1,48 @@
+package ArrayMatrix;
+
+public class spiralMatrix3Practice {
+    public static int[][] spiralMatrix3(int rows, int cols, int rStart, int cStart) {
+        int[][] result = new int[rows * cols][2];
+        int index = 0;
+        int r = rStart;
+        int c = cStart;
+        result[index++] = new int[]{r, c};
+        int[][] directions = {
+                {0, 1}, {1, 0}, {0, -1}, {-1, 0}
+        };
+        int stepLength = 1;
+        int dir = 0;
+
+
+        while (index < rows * cols) {
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < stepLength; j++) {
+                    r += directions[dir][0];
+                    c += directions[dir][1];
+
+                    if (r >= 0 && r < rows && c >= 0 && c < cols) {
+                        result[index++] = new int[]{r, c};
+                    }
+                }
+                dir = (dir + 1) % 4;
+            }
+            stepLength++;
+        }
+        return result;
+    }
+        public static void main (String[]args){
+            // Write your solution here
+            int rows = 5;
+            int cols = 6;
+            int rStart = 1;
+            int cStart = 4;
+
+            int[][] ans = spiralMatrix3(rows, cols, rStart, cStart);
+
+            System.out.println("Spiral Order Coordinates:");
+
+            for (int i = 0; i < ans.length; i++) {
+                System.out.println(ans[i][0] + " , " + ans[i][1]);
+            }
+        }
+    }
---
+++ src/ArrayMatrix/spiralMatrix4.java
@@ -0,0 +1,70 @@
+package ArrayMatrix;
+
+public class spiralMatrix4 {
+    public static int[][] spiralMatrix4(int m, int n, ListNode head) {
+        int[][] result = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++)
+                result[i][j] = -1;
+        }
+        int top = 0, bottom = m - 1, left = 0, right = n - 1;
+
+        while(head != null) {
+            // go right
+            for(int i = left; i <= right && head != null; i++) {
+                result[top][i] = head.val;
+                head = head.next;
+            }
+            top++;
+
+            // go down
+            for(int i = top; i <= bottom && head != null; i++) {
+                result[i][right] = head.val;
+                head = head.next;
+            }
+            right--;
+
+            // go left
+            for(int i = right; i >= left && head != null; i--) {
+                result[bottom][i] = head.val;
+                head = head.next;
+            }
+            bottom--;
+
+            // go up
+            for(int i = bottom; i >= top && head != null; i--) {
+                result[i][left] = head.val;
+                head = head.next;
+            }
+            left++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int m = 3;
+        int n = 5;
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(0);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(6);
+        head.next.next.next.next = new ListNode(8);
+        head.next.next.next.next.next = new ListNode(1);
+        head.next.next.next.next.next.next = new ListNode(7);
+        head.next.next.next.next.next.next.next = new ListNode(9);
+        head.next.next.next.next.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next.next.next.next.next = new ListNode(2);
+        head.next.next.next.next.next.next.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next.next.next.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next.next.next.next.next.next.next.next = new ListNode(0);
+
+        int[][] ans = spiralMatrix4(m, n, head);
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
